0d350c90099ead574684d1c1016d6a0b963d0861
miguelluvizon <miguel.luvizon@sptech.school>
2025-05-02 18:33:23 -0300

    Documentação da API: tratativa de erros feita

--- projetoPI/src/main/java/sptech/school/projetoPI/controllers/ClientController.java
+++ projetoPI/src/main/java/sptech/school/projetoPI/controllers/ClientController.java
@@ -1,6 +1,11 @@
 package sptech.school.projetoPI.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +19,8 @@ import sptech.school.projetoPI.dto.login.UserLoginDto;
 import sptech.school.projetoPI.dto.login.UserMapper;
 import sptech.school.projetoPI.dto.login.UserTokenDto;
 import sptech.school.projetoPI.entities.Client;
+import sptech.school.projetoPI.exceptions.ErroResponse;
+import sptech.school.projetoPI.exceptions.ErroResponseExamples;
 import sptech.school.projetoPI.services.user.ClientService;
 
 import java.util.List;
@@ -28,6 +35,10 @@ public class ClientController {
 
     @PostMapping
     @Operation(summary = "Cadastrar um cliente", description = "Cadastra um novo cliente no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Um ou mais campos estão inválidos"),
+    })
     public ResponseEntity<ClientResumeResponseDto> signClient(@Valid @RequestBody ClientRequestDto client) {
         Client tempClient = service.signClient(ClientMapper.toEntity(client));
         return ResponseEntity.status(201).body(ClientMapper.toResumeResponseDto(tempClient));
@@ -48,6 +59,23 @@ public class ClientController {
 
     @GetMapping
     @Operation(summary = "Buscar clientes", description = "Busca todos os clientes cadastrados no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Clientes trazidos com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.OK)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<List<ClientResumeResponseDto>> getAllClients() {
         List<Client> Clients = service.getAllClients();
 
@@ -60,12 +88,56 @@ public class ClientController {
 
     @GetMapping("/{id}")
     @Operation(summary = "Buscar cliente por ID", description = "Busca o cliente com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente encontrado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.OK)
+            )),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.NOT_FOUND)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<ClientResponseDto> getClientById(@PathVariable Integer id) {
         return ResponseEntity.status(200).body(ClientMapper.toResponseDto(service.getClientById(id)));
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "Atualizar cliente por ID", description = "Atualiza um cliente com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.OK)
+            )),
+            @ApiResponse(responseCode = "400", description = "Um ou mais campos estão inválidos", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.BAD_REQUEST)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<ClientResumeResponseDto> updateClientById(@Valid @RequestBody ClientRequestDto Client, @PathVariable Integer id) {
         Client tempClient = service.updateClientById(ClientMapper.toEntity(Client), id);
         return ResponseEntity.status(200).body(ClientMapper.toResumeResponseDto(tempClient));
@@ -73,6 +145,28 @@ public class ClientController {
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Deletar cliente por ID", description = "Deleta um cliente com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.NOT_FOUND)
+            )),
+            @ApiResponse(responseCode = "204", description = "Cliente removido com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.NO_CONTENT)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ClientResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<Void> deleteClientById(@PathVariable Integer id) {
         service.deleteClientById(id);
         return ResponseEntity.status(204).build();
--- projetoPI/src/main/java/sptech/school/projetoPI/controllers/ScheduleController.java
+++ projetoPI/src/main/java/sptech/school/projetoPI/controllers/ScheduleController.java
@@ -1,13 +1,20 @@
 package sptech.school.projetoPI.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sptech.school.projetoPI.dto.schedule.*;
+import sptech.school.projetoPI.dto.service.ServiceResumeResponseDto;
 import sptech.school.projetoPI.entities.Schedule;
+import sptech.school.projetoPI.exceptions.ErroResponseExamples;
 import sptech.school.projetoPI.services.ScheduleService;
 
 import java.util.List;
@@ -22,6 +29,18 @@ public class ScheduleController {
 
     @PostMapping
     @Operation(summary = "Cadastrar agendamento", description = "Cadastra um novo agendamento no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Agendamento cadastrado com sucesso!", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.CREATED)
+            )),
+            @ApiResponse(responseCode = "400", description = "Um ou mais campos estão inválidos!", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.BAD_REQUEST)
+            )),
+    })
     public ResponseEntity<ScheduleResumeResponseDto> signSchedule(@Valid @RequestBody ScheduleRequestDto schedule) {
         Schedule tempSchedule = service.signSchedule(ScheduleMapper.toEntity(schedule));
         return ResponseEntity.status(201).body(ScheduleMapper.toResumeResponseDto(tempSchedule));
@@ -29,6 +48,23 @@ public class ScheduleController {
 
     @GetMapping
     @Operation(summary = "Buscar agendamentos", description = "Busca todos os agendamentos cadastrados no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agendamentos trazidos com sucesso!", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.OK)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<List<ScheduleResumeResponseDto>> getAllSchedules() {
         List<Schedule> schedules = service.getAllSchedules();
 
@@ -41,12 +77,56 @@ public class ScheduleController {
 
     @GetMapping("/{id}")
     @Operation(summary = "Buscar agendamento por ID", description = "Busca o agendamento com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agendamento encontrado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.OK)
+            )),
+            @ApiResponse(responseCode = "404", description = "Agendamento não encontrado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.NOT_FOUND)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<ScheduleResponseDto> getScheduleById(@PathVariable Integer id) {
         return ResponseEntity.status(200).body(ScheduleMapper.toResponseDto(service.getScheduleById(id)));
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "Atualizar agendamento por ID", description = "Atualiza um agendamento com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agendamento atualizado com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.OK)
+            )),
+            @ApiResponse(responseCode = "400", description = "Um ou mais campos estão inválidos", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.BAD_REQUEST)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<ScheduleResumeResponseDto> updateScheduleById(@Valid @RequestBody ScheduleUpdateRequestDto schedule, @PathVariable Integer id) {
         Schedule tempSchedule = service.updateScheduleById(ScheduleMapper.toEntity(schedule), id);
         return ResponseEntity.status(200).body(ScheduleMapper.toResumeResponseDto(tempSchedule));
@@ -54,6 +134,28 @@ public class ScheduleController {
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Deletar agendamento por ID ", description = "Deleta um agendamento com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Agendamento não encontrado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.NOT_FOUND)
+            )),
+            @ApiResponse(responseCode = "204", description = "Agendamento removido com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.NO_CONTENT)
+            )),
+            @ApiResponse(responseCode = "403", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.FORBIDDEN)
+            )),
+            @ApiResponse(responseCode = "401", description = "Acesso não autorizado", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ScheduleResumeResponseDto.class),
+                    examples = @ExampleObject(value = ErroResponseExamples.UNAUTHORIZED)
+            ))
+    })
     public ResponseEntity<Void> deleteScheduleById(@PathVariable Integer id) {
         service.deleteScheduleById(id);
         return ResponseEntity.status(204).build();
